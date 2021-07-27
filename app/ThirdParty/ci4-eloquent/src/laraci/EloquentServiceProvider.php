24eca08f3d8e414d0a424a7932be9e5c95502c23
trianayulianto <triana.yulianto2018@gmail.com>
2021-07-27 18:38:20 +0700

    eloquent db provider

--- app/ThirdParty/ci4-eloquent/src/laraci/EloquentServiceProvider.php
+++ app/ThirdParty/ci4-eloquent/src/laraci/EloquentServiceProvider.php
@@ -5,7 +5,6 @@ namespace Fluent\Laraci;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher as Dispatcher;
 use Illuminate\Container\Container as Container;
-use PDO;
 
 class EloquentServiceProvider 
 {
@@ -61,19 +60,7 @@ class EloquentServiceProvider
             'password'  => $db->default['password'],
             'charset'   => $db->default['charset'],
             'collation' => $db->default['DBCollat'],
-            'prefix'    => $db->default['DBPrefix'],
-            'options'   => [
-                // Turn off persistent connections
-                PDO::ATTR_PERSISTENT => false,
-                // Enable exceptions
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                // Emulate prepared statements
-                PDO::ATTR_EMULATE_PREPARES => true,
-                // Set default fetch mode to array
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                // Set character set
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
-            ]
+            'prefix'    => $db->default['DBPrefix']
         );
     }
 }
