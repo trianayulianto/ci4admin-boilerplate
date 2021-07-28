09b17cfe58c1878db2ed3aa45b953a5d4dc43b11
trianayulianto <triana.yulianto2018@gmail.com>
2021-07-29 05:54:18 +0700

    User seeder

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -23,8 +23,7 @@ class UserSeeder extends Seeder
 	    $this->db->table('role_user')
 	        ->insert([
 	            'role_id' => 1,
-	            'user_id' => 1,
-	            'value'   => 1
+	            'user_id' => 1
 	       ]);
 	}
 }
