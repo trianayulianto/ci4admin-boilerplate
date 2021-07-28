7df5f2aeeea7dc58fec39fee592e52381d1ec5b1
trianayulianto <triana.yulianto2018@gmail.com>
2021-07-29 05:44:44 +0700

    User seeder

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+	public function run()
+	{
+		$data = [ 
+		    'name' => 'superuser',
+		    'email' => 'superuser@mail.test',
+		    'password' => password_hash('password', PASSWORD_BCRYPT),
+		    'email_verified_at' => date('Y-m-d H:i:s'),
+		    'remember_token' => rand(),
+			'created_at' => date("Y-m-d H:i:s")
+	    ];
+	    
+	    $this->db->table('users')
+	        ->insert($data);
+	    
+	    $this->db->table('role_user')
+	        ->insert([
+	            'role_id' => 1,
+	            'user_id' => 1,
+	            'value'   => 1
+	       ]);
+	}
+}
