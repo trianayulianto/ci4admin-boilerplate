a34b2314ea6a279be4da1294134a146cc00f148e
Triana Yulianto <triana.yulianto@tonjoo.com>
2021-11-30 15:19:51 +0700

    fix font not pushed

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,85 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{14:function(n,o){},3:function(n,o,w){w(4),n.exports=w(14)},4:function(n,o,w){w(5),w(13)},5:function(n,o,w){window._=w(6);try{window.Popper=w(2).default,window.$=window.jQuery=w(0),window.Cookies=w(8),w(9),w(10),w(11)($)}catch(n){}}},[[3,1,2]]]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/app"],{
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+
+__webpack_require__(/*! startbootstrap-sb-admin-2/js/sb-admin-2 */ "./node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js");
+
+/***/ }),
+
+/***/ "./resources/js/bootstrap.js":
+/*!***********************************!*\
+  !*** ./resources/js/bootstrap.js ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+
+try {
+  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"];
+  window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+  window.Cookies = __webpack_require__(/*! js-cookie */ "./node_modules/js-cookie/src/js.cookie.js");
+
+  __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
+
+  __webpack_require__(/*! jquery-easing */ "./node_modules/jquery-easing/dist/jquery.easing.1.3.umd.js"); // datatables
+
+
+  __webpack_require__(/*! datatables.net-bs4 */ "./node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js")($);
+} catch (e) {}
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+// window.axios = require('axios');
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+// import Echo from 'laravel-echo';
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 0:
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /home/tonjoo/htdocs/ci4admin-boilerplate/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/tonjoo/htdocs/ci4admin-boilerplate/resources/sass/app.scss */"./resources/sass/app.scss");
+
+
+/***/ })
+
+},[[0,"/js/manifest","/js/vendor"]]]);
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,44 @@
-window.refreshTokenExpiration=new Date((new Date).getTime()+36e5),window.onload=(()=>{cookieStore.get("token").then(e=>{null!==e&&Date.now()>e.expires&&$.ajax({url:"/refresh",type:"POST",success:e=>{Cookies.set("token",e.access_token,{expires:refreshTokenExpiration})}})})}),$(window).resize(function(){if($(window).width()<992)return $("#accordionSidebar").addClass("toggled"),$(window).width()<768&&$("#page-top").addClass("sidebar-toggled"),!1;$("#accordionSidebar").removeClass("toggled"),$("#page-top").removeClass("sidebar-toggled")}),$(window).width()<992&&($(window).width()<768&&$("#page-top").addClass("sidebar-toggled"),$("#accordionSidebar").addClass("toggled"));
+// set refresh token expiration global
+window.refreshTokenExpiration = new Date(new Date().getTime() + 60 * 60 * 1000); // 15 minutes
+
+// get refresh token
+window.onload = () => {
+	cookieStore.get('token').then((cookie) => {
+    if (cookie === null) { return; }
+
+		if (Date.now() > cookie.expires) {
+			$.ajax({
+				url: '/refresh',
+				type: 'POST',
+				success: (response) => {
+					Cookies.set('token', response.access_token, { 
+						expires: refreshTokenExpiration 
+					});
+				}
+			});
+		}
+    });
+}
+
+$(window).resize(function () {
+    if ($(window).width() < 992) {
+        $('#accordionSidebar').addClass('toggled');
+        
+        if ($(window).width() < 768) {
+            $('#page-top').addClass('sidebar-toggled');
+        }
+        
+        return false;
+    }
+    
+    $('#accordionSidebar').removeClass('toggled');
+    $('#page-top').removeClass('sidebar-toggled');
+});
+
+if ($(window).width() < 992) {
+    if ($(window).width() < 768) {
+        $('#page-top').addClass('sidebar-toggled');
+    }
+    
+    $('#accordionSidebar').addClass('toggled');
+}
